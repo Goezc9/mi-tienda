092954c4afaa4dee4ecb34f7a15d1dd926dfa364
Goezc9 <rodriguezgoezmariacamila25@gmail.com>
2026-04-25 00:13:09 -0500

    react router limpio

--- mi-tienda/src/App.jsx
+++ mi-tienda/src/App.jsx
@@ -1,24 +1,22 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/acerca-de" element={<About />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/inicio" element={<Home />} />
+          <Route path="/acerca-de" element={<About />} />
+        </Routes>
+      </main>
+    </>
   )
 }
+
 
 
 
